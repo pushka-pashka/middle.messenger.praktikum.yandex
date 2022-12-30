1b3a5b1ed23d57c3528c3609d5be472727cfb321
pushka-pashka <pasheka80389@ya.ru>
2022-12-30 18:49:27 +0300

    настройка окружения и исправление ошибок линтера

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require('express');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/dist'));
-
-app.listen(PORT, () =>
-    console.log(`App listening to port ${PORT}.`)
-);
+app.use(express.static(`${__dirname}/dist`));
+// eslint-disable-next-line no-console
+app.listen(PORT, () => console.log(`App listening to port ${PORT}.`));
--- handlebars.config.js
+++ handlebars.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    layouts: './src/layouts',
-    pages: './src/pages',
-    partials: './src/components'
+  layouts: './src/layouts',
+  pages: './src/pages',
+  partials: './src/components'
 };
